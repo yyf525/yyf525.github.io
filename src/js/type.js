e2e7c4536faa8315978701a51fb2efeec05c5776
yyf525 <114897714+yyf525@users.noreply.github.com>
2022-10-16 20:03:39 +0800

    Update type.js

--- src/js/type.js
+++ src/js/type.js
@@ -1,7 +1,7 @@
 (function () {
     var a_idx = 0;
     window.onclick = function (event) {
-        var a = new Array("❤", "点赞！", "强！", "厉害", "懂了！");
+        var a = new Array("❤", "点赞！", "强！", "厉害", "原来如此", "懂了", "感谢博主");
 
         var heart = document.createElement("b"); //创建b元素
         heart.onselectstart = new Function('event.returnValue=false'); //防止拖动
